fd3610efb8f95c17a3fefa02f3d33013316d1d4a
jianbo.wang <jianbo.wang@cygia.com>
2021-09-27 17:46:41 +0800

    增加相机相关项目文件夹

---
+++ Vision/Cameras/Vision.Camera.Hikrobot/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vision.Camera.Hikrobot
+{
+    public class Class1
+    {
+    }
+}
